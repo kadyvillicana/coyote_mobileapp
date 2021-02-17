47227546677e832cb5d697f5b71394debe6a6192
Kady <Villicana>
2021-02-16 22:11:03 -0600

    added custom header

---
+++ components/custom-header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import CustomText from './custom-text';
+
+const CustomHeader = ({header, subHeader}) => {
+  return (
+  <View style={[styles.headerContainer]}>
+    <View>
+      <CustomText fontSize='big' fontType='bold'>{header}</CustomText>
+      <View style={{marginTop: 15}}>
+        <CustomText
+          secondaryColor 
+          fontSize='medium'>{subHeader}</CustomText>
+      </View>
+    </View>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    paddingTop: 30,
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15,
+    paddingBottom: 30,
+    flexDirection: 'row',
+},
+})
+
+
+export default CustomHeader;
--- screens/add-car.js
+++ screens/add-car.js
@@ -1,32 +1,16 @@
 import React, { useState } from 'react';
 import CustomDropDown from '../components/custom-dropdown';
+import CustomHeader from '../components/custom-header';
 import CustomText from '../components/custom-text';
 import CustomView from '../components/custom-view';
 
-const makes = require('../data/static/makes.json');
-
 const AddCarScreen = () => {
-
-  const [make, setMake] = useState(null);
-
-  if(make){
-    return(
-      <CustomView>
-        <CustomText>
-          {make.makeName}
-        </CustomText>
-      </CustomView>
-    )
-  }
 
   return(
     <CustomView>
-      <CustomText>
-        Agrega un auto
-      </CustomText>
-      <CustomDropDown 
-        data={makes}
-        onItemSelected={setMake}
+      <CustomHeader 
+        header='Agrega un auto'
+        subHeader=''
       />
     </CustomView>
   )
